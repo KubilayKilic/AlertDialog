5eca2aea38be6e0c44422576b7d7ca29150fd965
SwordMaster3000 <kubilaykilich@gmail.com>
2020-02-27 20:04:57 +0300

    Simple alert Message with toast

---
+++ app/src/main/java/com/kaan/alertdialog/MainActivity.java
@@ -0,0 +1,55 @@
+package com.kaan.alertdialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Context
+        //Activity Context
+
+        //App Context
+
+        //getApplicationContext()
+
+        //aktivite ile ilgili durumlarda getapplicationcontext de mainacticity.this de kullanılabilir.
+
+        Toast.makeText(MainActivity.this, "Toast Message", Toast.LENGTH_LONG).show();
+    }
+
+
+    public void save(View view) {
+        //alert dialog kullanarak uyarı mesajı göstermek
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Save");
+        alert.setMessage("Are you sure?");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //save
+                Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Not Saved", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alert.show();
+    }
+}
